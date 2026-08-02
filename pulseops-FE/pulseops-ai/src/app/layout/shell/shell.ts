eb8eb86f13100dfc3bc28db00a6df6d4809286f4
suraganimukesh34 <suraganimukesh34@gmail.com>
2026-08-02 12:02:57 +0530

    feat: create application shell components

---
+++ pulseops-FE/pulseops-ai/src/app/layout/shell/shell.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shell',
+  imports: [],
+  templateUrl: './shell.html',
+  styleUrl: './shell.scss',
+})
+export class Shell {}
